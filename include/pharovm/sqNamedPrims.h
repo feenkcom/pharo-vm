3a8a1c971db3973524d0409ec3f490ca895ed3ba
Aliaksei Syrel <alex.syrel@gmail.com>
2023-01-28 19:23:52 +0100

    fix pluginExport definition

--- include/pharovm/sqNamedPrims.h
+++ include/pharovm/sqNamedPrims.h
@@ -1,9 +1,13 @@
-extern sqExport vm_exports[];
-extern sqExport os_exports[];
+#pragma once
 
-sqExport *pluginExports[] = {
-	vm_exports,
-	os_exports,
-//	SecurityPlugin_exports,
-	NULL
-};
+#include "exportDefinition.h"
+
+typedef struct {
+  char *pluginName;
+  char *primitiveName; /* N.B. On Spur the accessorDepth is hidden after this */
+  void *primitiveAddress;
+} sqExport;
+
+EXPORT(sqExport) vm_exports[];
+EXPORT(sqExport) os_exports[];
+EXPORT(sqExport*) pluginExports[];
